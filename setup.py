ec5d41dd9497c89facf8466a3229ffd32a980b75
unknown <xav.godon@gmail.com>
2019-05-09 16:36:42 -0400

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ssxtd',
-    version='1.0.0',
+    version='1.0.1',
     packages=['ssxtd',],
     license='MIT',
     author='Xavier Godon',
